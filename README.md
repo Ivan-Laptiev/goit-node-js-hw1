dad8f3ef6f68d4626c38ff818420f52907e221d9
Ivan-Laptiev <laptiev.ivan.j@gmail.com>
2022-12-08 01:12:16 +0200

    Done

---
+++ README.md
@@ -0,0 +1,11 @@
+Contacts List (node index.js --action list)
+https://monosnap.com/file/DGADWfGeY62GYI7TSHV6l7akUMxAFv
+
+Get Contact by id (node index.js --action get --id=6)
+https://monosnap.com/file/HmHPaDl7IrQyvI89zTV5N9w3QMyUYL
+
+Add contact to Contacts List(node index.js --action add --name Mango --email mango@gmail.com --phone 658-22-22)
+https://monosnap.com/file/3W8FdW8DaPHRi4Xee48IP9VWXY6EWE
+
+Remove Contact from Contacts List(node index.js --action remove --id=2)
+https://monosnap.com/file/ia5AZB9VDiZqMMmXuaeUeizHthUKGQ
--- contacts.js
+++ contacts.js
@@ -1,21 +1,62 @@
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const fs = require("fs/promises");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-    // ...твой код
+  const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+   try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts= JSON.parse(data);  
+    return contacts;
+    } catch(error) {
+    console.log(error.message);
+    }   
   }
+    
+  async function getContactById(contactId) {
+    try{
+    const contacts= await listContacts();
+    const contactWithId = contacts.find(({id})=> id === contactId);
+    if(!contactWithId) {
+      return null;
+    }
+    return contactWithId;    
+    } catch (error){
+      console.log(error.message);
+    }
+  }
+    
+  async function removeContact(contactId) {
+    try {
+    const contacts= await listContacts();
+    const removedContactId = contacts.findIndex((item)=> item.id === contactId);
+    if (removedContactId === -1) {
+      return null;
+    }
+    const uppdatedContacts = contacts.filter(({id})=> id !== contactId );
+    await fs.writeFile(contactsPath, JSON.stringify(uppdatedContacts));
+    return contacts[removedContactId];
+    } catch (error){
+      console.error(error);
+    }
+  } 
   
-  function getContactById(contactId) {
-    // ...твой код
+  async function addContact(name, email, phone) {
+   try {   
+    const newContact = { id: uuidv4(), name, email, phone};
+    const contacts = await listContacts();
+    const updatedContacts = [...contacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts), "utf8");
+    return newContact; 
+   } catch (error){
+    console.log(error);
+   }
   }
-  
-  function removeContact(contactId) {
-    // ...твой код
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твой код
-  }
+
+  module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+  };
